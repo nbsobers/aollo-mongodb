baec517124506915d1c634f2054693b637f9aa1b
Sobers <Sobers.Bhaskaran@damanhealth.ae>
2021-10-17 15:38:28 +0400

    .

--- src/index.js
+++ src/index.js
@@ -35,6 +35,6 @@ import { PORT } from "../config/config";
   });
 
   // Modified server startup
-  await new Promise((resolve) => httpServer.listen({ port: process.env.PORT || PORT }, resolve));
-  console.log(`🚀 Server ready at http://localhost:${process.env.PORT || PORT}${server.graphqlPath}`);
+  await new Promise((resolve) => httpServer.listen({ port: process.env.PORT || 3000 }, resolve));
+  console.log(`🚀 Server ready at http://localhost:${process.env.PORT || 3000}${server.graphqlPath}`);
 })();
